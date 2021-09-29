e65e169b816323a012598a48fbee7b5d5be2c26c
carleonercs <carleone.souza@rcs.med.br>
2021-09-29 15:59:11 -0300

    Project hr-eureka-server created

--- hr-payroll/src/main/java/com/projectscps/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/projectscps/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,12 @@ package com.projectscps.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @EnableFeignClients
+@LoadBalancerClient(name = "hr-worker")
 @SpringBootApplication
 public class HrPayrollApplication {
 
--- hr-payroll/src/main/java/com/projectscps/hrpayroll/config/AppConfig.java
+++ hr-payroll/src/main/java/com/projectscps/hrpayroll/config/AppConfig.java
@@ -1,14 +1,19 @@
 package com.projectscps.hrpayroll.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Configuration
+@LoadBalancerClient(name = "hr-worker")
 public class AppConfig {
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+	  @LoadBalanced
+	  @Bean
+	  WebClient.Builder webClientBuilder() {
+	    return WebClient.builder();
+	  }
 }
